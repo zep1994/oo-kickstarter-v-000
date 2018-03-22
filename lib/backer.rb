c54066708cb350cc1e0644575f5e035aca0f05c4
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-22 16:04:51 +0000

    automatically backed up by learn

---
+++ lib/backer.rb
@@ -0,0 +1,7 @@
+class Backer
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+end
---
+++ lib/project.rb
@@ -0,0 +1,7 @@
+class Project
+     attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+end
