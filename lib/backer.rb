2ab195a4575fa14bf8ae15bc49554166ba7caa2d
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-22 16:21:34 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,7 +7,7 @@ class Backer
   end
   
   def back_project(project)
-  @backed_projects << project
-  project.backers << self
+    @backed_projects << project
+    project.backers << self
   end
 end
